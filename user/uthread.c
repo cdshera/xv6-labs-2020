6642c9d5a4852b4a785413ca4fedd820eddec1ad
cdshera <zlj_617@163.com>
2024-10-05 17:35:44 +0800

    lab7:thread 1st step

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,31 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+//lab7:thread 1st step
+struct context
+{
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct     context context;       /*上下文，callee寄存器*/ //lab7:thread 1st step
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +83,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context, (uint64)&next_thread->context);  //lab7:thread 1st step
+
   } else
     next_thread = 0;
 }
@@ -77,6 +99,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  memset(&t->context, 0, sizeof(struct context));
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack + STACK_SIZE; // 栈由高地址向低地址扩展
 }
 
 void 
